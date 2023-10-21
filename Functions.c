b94e29c484f3a40d7cf860d5e5d7be2678b8b321
vmromanov <145842293+vmromanov@users.noreply.github.com>
2023-10-21 12:45:00 +0300

    Add files via upload

--- Functions.c
+++ Functions.c
@@ -1,19 +1,19 @@
 #include "Functions.h"
+#include <stdbool.h>
+unsigned long long Square(const long long value_)
+{
+   return value_ * value_;
+}
 
-//unsigned long long Square(const long long value_)
-//{
-//   return value_ * value_;
-//}
-//
-//bool Prime(unsigned long long int x)
-//{
-//	if (x % 2 == 0)
-//		return false;
-//
-//	for (unsigned int n = 3; n < sqrt((double)x) + 1; n += 2)
-//	{
-//		if (x % 2 == 0)
-//			return false;
-//	}
-//	return true;
-//}
+bool Prime(unsigned long long int x)
+{
+	if (x % 2 == 0)
+		return false;
+
+	for (unsigned int n = 3; n < sqrt((double)x) + 1; n += 2)
+	{
+		if (x % 2 == 0)
+			return false;
+	}
+	return true;
+}
--- Functions.h
+++ Functions.h
@@ -1,17 +1,17 @@
-//#pragma once
-//#ifndef _FUNCTIONS_H_
-//#define _FUNCTIONS_H_ 
-//
-///*
-//* @brief возводит число в квадрат.
-//* @paqram value_ число.
-//* @reurn квадрат числа.
-//*/
-//unsigned long long Square(const long long value_);
-//
-//#endif
-//#pragma once
-//#include <stdio.h>
-//#include<stdbool.h>
-//bool Prime(unsigned long long int x);
+#pragma once
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_ 
+
+/*
+* @brief возводит число в квадрат.
+* @paqram value_ число.
+* @reurn квадрат числа.
+*/
+unsigned long long Square(const long long value_);
+
+#endif
+#pragma once
+#include <stdio.h>
+#include<stdbool.h>
+bool Prime(unsigned long long int x);
 
